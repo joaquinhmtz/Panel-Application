0abb9cd7a4ae4b889e1c8e42de40a20c1ae66747
Joaquin Hernandez <joaquin.hernandez@hovahealth.com>
2019-04-09 12:19:35 -0500

    Cambios menores

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var cors = require('cors');
 var bodyParser = require('body-parser');
 var helmet = require('helmet');
 var port = process.env.PORT || 3005;
-var ip = process.env.PANEL_IP;
 require('dotenv').config();
 
 app.use(express.static(__dirname + '/public'));
@@ -14,7 +13,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(cors());
 app.use(helmet());
 
-mongoose.connect('mongodb://' + 'ip' + '/panel', { useNewUrlParser: true }, function (err) {
+mongoose.connect('mongodb://localhost/panel', { useNewUrlParser: true }, function (err) {
     if (err) return console.log('Error al conectarse a la base', err);
     console.log('Conexión a la base exitosa');
 });
